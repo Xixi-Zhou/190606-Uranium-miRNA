6c84b46775d95617771c1c74e0a0d40ba0f64a93
Xixi Zhou <xzhou@salud.unm.edu>
2019-08-18 22:41:47 -0600

    Revised R script for readability.

--- miRNA_Network.R
+++ miRNA_Network.R
@@ -1,117 +1,66 @@
-# Purpose:
-# predict miRNA-mRNA interactions;
-# perform GO enrichment;
-# analyze reactome pathways.
+# Predict miRNA-mRNA interactions, perform GO enrichment, 
+# & analyze reactome pathways
 # Xixi Zhou, UNM CoP, Dept. of PS: xzhou@salud.unm.edu
+# Load packages, 'walk' and 'map' are not available for now.
+sapply(c("tidyverse", "ggpubr", "rio",
+         "miRNAtap", "topGO", "ReactomePA"), 
+       library, character.only = T)
+# Read miRNA targets from excel spreadsheet, up- and down-regulated.
+miRNAs <- import("Data.xlsx") %>% as_tibble()
 
-# Load packages:
-library(miRNAtap) # mRNA prediction for miRNA targets
-library(topGO) # Gene Ontology enrichment
-library(ReactomePA) # Reactome pathway analysis
-library(org.Mm.eg.db) # Genome wide annotation for Mouse
-library(tidyverse) # Basic data science package
-library(readxl) # Read excel spredsheets
-library(ggpubr) # Generate figures for publication
-
-# Read miRNA targets from two txt files, up- and down-regulated.
-miRNAs <- read_xlsx("Data.xlsx", sheet = "miRNAs", col_names = T)
-
-# 1. Predict mRNA targets:
-predictions <- lapply(miRNAs$miRNA,
-                      getPredictedTargets, # Function to predict mRNA
-                      species = "mmu", # Mouse
-                      min_src = 2, # at least from 2 sources
-                      method = "geom") # geom method by default
-# Convert matrix to tibble, add gene ID as a new column
-predictions <- lapply(predictions, as_tibble, rownames = "mRNA")
-# Add annotations
-predictions <- mapply(add_column, # Add annotations to results
-                      predictions,
-                      miRNA = miRNAs$miRNA, # miRNA name
-                      regulation = miRNAs$regulation) # up or down
-# Combine into a single tibble
-predictions <- do.call("bind_rows", predictions) 
-
+# 1. Predict mRNA targets, and save -----
+predictions <-
+  map(miRNAs$miRNA, getPredictedTargets, # Function to predict mRNA
+      species = "mmu", # Mouse
+      min_src = 2, # at least from 2 sources
+      method = "geom") %>%
+  map(as_tibble, rownames = "mRNA") %>% # Data wrangling
+  list(miRNAs$miRNA, miRNAs$regulation) %>%
+  pmap(~mutate(..1, miRNA = ..2, regulation = ..3)) %>% 
+  bind_rows() %T>% # !!! %T>% passes left hand site value back
+  export("mRNA_predictions.xlsx") # Save results to a spreadsheet
 # Filter up- and down-regulated mRNAs, store as seperated vectors
 # Rank value as the vector, mRNA names as vector name
-mRNA_list <- split(predictions, f = predictions$regulation)
-mRNA_list <- lapply(mRNA_list,
-                    function(x) {
-                      vector <- pull(x, rank_product)
-                      names(vector) <- pull(x, mRNA)
-                      vector})
+mRNA_list <- split(predictions, f = predictions$regulation) %>%
+  map(~{
+    vector <- pull(., rank_product)
+    names(vector) <- pull(., mRNA)
+    vector})
 
-# 2. GO enrichment of predicted genes
-selection <- function(x) {TRUE} # We don't impose a cut off
-all_GO2_genes <- annFUN.org(
-  whichOnto = "BP",
-  feasibleGenes = NULL,
-  mapping = "org.Mm.eg.db",
-  ID = "entrez")
-GO_data <- lapply(mRNA_list,
-                  function(x) {
-                    new('topGOdata', # Database
-                        ontology = 'BP', # Biological processes
-                        allGenes = x,
-                        annot = annFUN.GO2genes,
-                        GO2genes = all_GO2_genes,
-                        geneSel = selection,
-                        nodeSize = 10)})
-ks_results <- lapply(GO_data, runTest,
-                     algorithm = "classic",
-                     statistic = "ks")
-# Generate table of GO results
-GO_results <- mapply(function(x, y) GenTable(x,
-                                             KS = y,
-                                             orderBy = "KS",
-                                             topNodes = 50),
-                     GO_data,
-                     ks_results,
-                     SIMPLIFY = F) # Force to return a list
-# Clean up
-rm(all_GO2_genes, selection)
+# 2. GO enrichment of predicted genes -----
+GO_data <- map(mRNA_list, ~new(
+  'topGOdata', # Database
+  ontology = 'BP', # Biological processes
+  allGenes = ., annot = annFUN.GO2genes,
+  GO2genes = annFUN.org(
+    whichOnto = "BP", feasibleGenes = NULL,
+    mapping = "org.Mm.eg.db", ID = "entrez"),
+  geneSel = function(x) TRUE, # No filters
+  nodeSize = 10))
+ks_results <- map(GO_data, runTest, algorithm = "classic", statistic = "ks")
+# Generate table of GO results, and Save GO results
+# 'export' function can write the list of tables to multiple sheets
+map2(GO_data, ks_results,
+     ~GenTable(.x, KS = .y, orderBy = "KS", topNodes = 50)) %>% 
+  export(file = "GO_results.xlsx")
 
-# 3. Use predicted genes for reactome analysis
-pathways <- lapply(mRNA_list, function(x) {
-  enrichPathway(
-    gene = names(x),
-    organism = "mouse",
-    pvalueCutoff = 0.05, # Cut off at p<0.05
-    readable = T)})
+# 3. Use predicted genes for reactome analysis -----
+pathways <- map(mRNA_list, ~enrichPathway(
+  gene = names(.), organism = "mouse", pvalueCutoff = 0.05, readable = T))
 
-# 4. Save results and generate figures
+# 4. generate figures -----
 # Save data image
 save.image("miRNA_data.RData")
-# Save mRNA targets
-write.table(predictions,
-            file = "Results/Predictions.txt",
-            append = F,
-            row.names = F,
-            col.names = T,
-            sep = "\t",
-            dec = ".")
-# Save GO results
-mapply(function(x,y)
-  write.table(x,
-              file = paste("Results/GO_", y, ".txt", sep = ""),
-              append = F,
-              sep = "\t",
-              dec = ".",
-              row.names = F,
-              col.names = T), GO_results, names(GO_results))
-# plot device
-pdf("Figures.pdf", width = 18, height = 9, onefile = T)
-# Summary targets in a histogram
-gghistogram(predictions, x="miRNA", stat="count",
-            title="Predicted mRNA Targets of miRNAs",
-            fill="regulation", palette="lancet",
-            color=FALSE, xlab = "", ylab = "Count",
-            legend="bottom")
-# Plot GO and pathway figures
-mapply(function(x,y) {
-  showSigOfNodes(x, score(y), useInfo = 'all')},
-  GO_data, ks_results)
-lapply(pathways, dotplot, showCategory = 20)
-lapply(pathways, cnetplot, categorySize="pvalue")
-# Wrap up
+# Plot to pdf
+pdf("Figures.pdf", width = 16, height = 9, onefile = T)
+# Plot histogram of mRNA prediction
+gghistogram(
+  predictions, x="miRNA", stat="count",
+  title="Predicted mRNA Targets of miRNAs", fill="regulation",
+  palette="lancet", color=F, xlab = "", ylab = "Count", legend="bottom")
+# Plot GO enrichment results
+map2(GO_data, ks_results, ~showSigOfNodes(.x, score(.y), useInfo = 'all'))
+# Plot reactome analysis results
+map(pathways, dotplot, showCategory = 20)
+map(pathways, cnetplot, categorySize="pvalue")
 dev.off()
